9c82b383cdd8cfdbebfcc4dd699945f839c8f73a
Anbazhagan, Raamji(R.) <ranbazh3@ford.com>
2025-10-16 20:27:49 +0530

    Changes in Application

--- src/main/java/com/example/collectionRequirements/CollectionRequirementsB2Application.java
+++ src/main/java/com/example/collectionRequirements/CollectionRequirementsB2Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CollectionRequirementsB2Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CollectionRequirementsB2Application.class, args);
+        SpringApplication.run(CollectionRequirementsB2Application.class, args);
 	}
 
 }
