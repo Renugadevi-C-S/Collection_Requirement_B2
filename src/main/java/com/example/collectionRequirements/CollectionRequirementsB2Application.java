5a4afdf7f3de27ad50733aaf9aaae9e272201eff
Anbazhagan <ranbazh3@ford.com>
2025-10-16 11:22:11 +0530

    Initial Commit

---
+++ src/main/java/com/example/collectionRequirements/CollectionRequirementsB2Application.java
@@ -0,0 +1,13 @@
+package com.example.collectionRequirements;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectionRequirementsB2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollectionRequirementsB2Application.class, args);
+	}
+
+}
