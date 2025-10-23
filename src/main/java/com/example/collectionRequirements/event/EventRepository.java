3149878f95da948a8185e1c7df5b955d79c7bd8b
C S, Renugadevi (R.) <rcs3@ford.com>
2025-10-23 10:58:54 +0530

    Implemented EventRepository and EventService

--- src/main/java/com/example/collectionRequirements/event/EventRepository.java
+++ src/main/java/com/example/collectionRequirements/event/EventRepository.java
@@ -5,7 +5,10 @@ import com.example.department.Department;
 import com.example.region.Region;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 
 public interface EventRepository extends JpaRepository<Event, Long> {
-
+    List<Event> findByStatus (String status);
+    List<Event> findByEventType (String eventType);
 }
--- src/main/java/com/example/collectionRequirements/event/EventService.java
+++ src/main/java/com/example/collectionRequirements/event/EventService.java
@@ -1,5 +1,14 @@
 package com.example.collectionRequirements.event;
+import java.util.List;
 
 public interface EventService {
+    Event createEvent(Event newEvent) throws EventException;
 
+    Event getEventById(Long eventId) throws EventException;
+
+    List<Event> getAllEvents() throws EventException;
+
+    Event editEvent(Long eventId, Event updateEvent) throws EventException;
+
+    void deleteEvent(Long eventId) throws EventException;
 }
