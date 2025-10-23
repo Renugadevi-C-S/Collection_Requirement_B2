905f3d87ef544cffe1d8f871d085e45588bdaf4b
MPRADE11 <pradeepasundar115@gmail.com>
2025-10-23 10:23:22 +0530

    added crud to department region and userinfo and added json ignore

--- src/main/java/com/example/region/Region.java
+++ src/main/java/com/example/region/Region.java
@@ -1,6 +1,7 @@
 package com.example.region;
 
 import com.example.user.UserInfo;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
@@ -51,6 +52,7 @@ public class Region {
     }
 
     @OneToMany(mappedBy = "region")
+    @JsonIgnore
     private List<UserInfo> users;
 
 
