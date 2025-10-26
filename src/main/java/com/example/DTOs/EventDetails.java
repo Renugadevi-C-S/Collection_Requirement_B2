3674782d99fb723a72d46e834a111f6e5a0b59a3
C S, Renugadevi (R.) <rcs3@ford.com>
2025-10-26 23:11:54 +0530

    Added EventDetails DTO to receive from frontend

---
+++ src/main/java/com/example/DTOs/EventDetails.java
@@ -0,0 +1,67 @@
+package com.example.DTOs;
+
+public class EventDetails {
+    private String eventName;
+    private String description;
+    private Integer participantsCount;
+    private Integer duration;
+    private String eventType;
+    private String fundingSource;
+    private String status;
+
+    public String getEventName() {
+        return eventName;
+    }
+
+    public void setEventName(String eventName) {
+        this.eventName = eventName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getParticipantsCount() {
+        return participantsCount;
+    }
+
+    public void setParticipantsCount(Integer participantsCount) {
+        this.participantsCount = participantsCount;
+    }
+
+    public Integer getDuration() {
+        return duration;
+    }
+
+    public void setDuration(Integer duration) {
+        this.duration = duration;
+    }
+
+    public String getEventType() {
+        return eventType;
+    }
+
+    public void setEventType(String eventType) {
+        this.eventType = eventType;
+    }
+
+    public String getFundingSource() {
+        return fundingSource;
+    }
+
+    public void setFundingSource(String fundingSource) {
+        this.fundingSource = fundingSource;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
