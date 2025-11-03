a78d2e6c6224b3a756f814879100cd65a7c02b2d
C S, Renugadevi (R.) <rcs3@ford.com>
2025-11-03 20:02:45 +0530

    removed unwanted implementation.

--- src/main/java/com/example/collectionRequirements/event/EventRepository.java
+++ src/main/java/com/example/collectionRequirements/event/EventRepository.java
@@ -1,9 +1,6 @@
 package com.example.collectionRequirements.event;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-
 import java.util.List;
 
 
