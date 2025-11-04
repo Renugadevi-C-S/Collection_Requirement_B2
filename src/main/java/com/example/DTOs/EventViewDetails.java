d30d888a83a2784dce2e7d9c1bc836aa6cca37cd
C S, Renugadevi (R.) <rcs3@ford.com>
2025-11-04 15:59:54 +0530

    Added new fields

--- src/main/java/com/example/DTOs/EventViewDetails.java
+++ src/main/java/com/example/DTOs/EventViewDetails.java
@@ -1,12 +1,18 @@
 package com.example.DTOs;
 
+import java.util.List;
+
 public class EventViewDetails {
     private Long eventId;
     private String eventName;
+    private String description;
     private Integer duration;
     private String eventType;
+    private String fundingSource;
+    private Integer participantsCount;
     private String status;
     private String createdBy;
+    private List<RequestsViewDetails> linkedRequests;
 
     public Long getEventId() {
         return eventId;
@@ -22,6 +28,14 @@ public class EventViewDetails {
 
     public void setEventName(String eventName) {
         this.eventName = eventName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 
     public Integer getDuration() {
@@ -40,6 +54,22 @@ public class EventViewDetails {
         this.eventType = eventType;
     }
 
+    public String getFundingSource() {
+        return fundingSource;
+    }
+
+    public void setFundingSource(String fundingSource) {
+        this.fundingSource = fundingSource;
+    }
+
+    public Integer getParticipantsCount() {
+        return participantsCount;
+    }
+
+    public void setParticipantsCount(Integer participantsCount) {
+        this.participantsCount = participantsCount;
+    }
+
     public String getStatus() {
         return status;
     }
@@ -54,5 +84,13 @@ public class EventViewDetails {
 
     public void setCreatedBy(String createdBy) {
         this.createdBy = createdBy;
+    }
+
+    public List<RequestsViewDetails> getLinkedRequests() {
+        return linkedRequests;
+    }
+
+    public void setLinkedRequests(List<RequestsViewDetails> linkedRequests) {
+        this.linkedRequests = linkedRequests;
     }
 }
