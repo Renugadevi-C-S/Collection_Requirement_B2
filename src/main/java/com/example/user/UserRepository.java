35f5f5df43efaa249f6a484bc5af55205b5dacd6
Anbazhagan, Raamji <ranbazh3@ford.com>
2025-11-05 18:06:05 +0530

    Added to get multiple users with List of cdsIds

--- src/main/java/com/example/user/UserRepository.java
+++ src/main/java/com/example/user/UserRepository.java
@@ -3,10 +3,13 @@ package com.example.user;
 import com.example.collectionRequirements.approval.Approval;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<UserInfo, Long> {
     Optional<UserInfo> findByCdsID(String cdsID);
 
     boolean existsByCdsID(String cdsId);
+
+    List<UserInfo> findByCdsIDIn(List<String> usersCdsId);
 }
